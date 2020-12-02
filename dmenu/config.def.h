e45a39a00c188402d050eeb798b4806472e4a48f
Faro <74023273+faro-r@users.noreply.github.com>
2020-12-02 14:12:17 +0100

    Add files via upload

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -6,7 +6,8 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 100;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira Code:pixelsize=14:antialias=true:autohint=true"
+	"Fira Code:pixelsize=14:antialias=true:autohint=true",
+    "Liberation Mono:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
